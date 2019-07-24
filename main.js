612a1607376ab47b0a35f64d68ee4d8e28bc1ca4
xiuqiang1995 <a18805819821@gmail.com>
2019-07-25 02:27:17 +0800

    船新版本 但是有个bug 如果快速在第一张和最后一张之间切换会出问题（假的还没来得及变成真的）

--- main.js
+++ main.js
@@ -1,46 +1,71 @@
-var allButtons = $('#buttons > span');
-for (let index = 0; index < allButtons.length; index++) {
-    $(allButtons[index]).on('click', function(e) {
-        //jq api index() 获取当前选中元素是兄弟中的第几个
-        var i = $(e.currentTarget).index();
-        var n = i * -300;
-        $('#images').css({
-            transform: 'translate(' + n + 'px)'
-        })
-        m = i;
-        //给按钮增加激活的样式
-        activeBtn(allButtons.eq(m));
+let $buttons = $('#buttonWrapper>button');
+let $slides = $('#slides');
+//获取所有幻灯片
+let $images = $slides.children('img')
+    //假的第一页
+let $firstCopy = $images.eq(0).clone(true);
+//假的最后一页
+let $lastCopy = $images.eq($images.length - 1).clone(true);
+//假的第一页加到最后一页后面
+$slides.append($firstCopy);
+//假的最后一页加到第一页前面
+$slides.prepend($lastCopy);
+//幻灯片整体左移275px，不然第一页显示的是假的最后一页
+$slides.css({ transform: 'translateX(-275px)' })
+    //当前页下标
+let current = 0;
+
+//上一页、下一页
+$(next).on('click', function() {
+    goToSlide(current + 1);
+})
+$(previous).on('click', function() {
+        goToSlide(current - 1);
     })
-}
-//自动轮播
-var m = 1;
-activeBtn(allButtons.eq(0));
-var size = allButtons.length;
-var timerId = setTimer();
-//鼠标移入时，暂停轮播
-$('.window').on('mouseenter', function() {
-        window.clearInterval(timerId);
-    })
-    //鼠标移出时，恢复轮播
-$('.window').on('mouseleave', function() {
-    timerId = setTimer();
+    //自动轮播
+var timer = setInterval(function() {
+        goToSlide(current + 1)
+    }, 2000)
+    //鼠标移入暂停、移出恢复
+$('.container').on('mouseenter', function() {
+    window.clearInterval(timer);
+}).on('mouseleave', function() {
+    timer = setInterval(function() {
+        goToSlide(current + 1)
+    }, 2000)
 })
 
-function activeBtn($btn) {
-    //siblings('.red') 接收一个选择器，找到具有该选择器兄弟节点
-    //.removeClass('red') 移除它的red属性
-    $btn.addClass('red').siblings('.red').removeClass('red');
-}
+$('#buttonWrapper').on('click', 'button', function(e) {
+    let $button = $(e.currentTarget);
+    let index = $button.index();
+    goToSlide(index);
+})
 
-function playSlide(index) {
-    //eq(n % 3) 找到第 n%3 个元素，并包装成jq对象
-    allButtons.eq(index).trigger('click')
-}
-
-function setTimer() {
-    return setInterval(() => {
-        console.log(new Date());
-        playSlide(m % size);
-        m++;
-    }, 1000)
+function goToSlide(index) {
+    if (index > $images.length - 1) {
+        index = 0;
+    } else if (index < 0) {
+        index = $images.length - 1;
+    }
+    if (current === $images.length - 1 && index === 0) {
+        //从最后一张到第一张,先到假的第一张上，再隐藏幻灯片，再瞬间切换到真的第一张,再显示幻灯片
+        //为什么要用 ` `
+        $slides.css({ transform: `translateX(${- (index+4) * 275}px)` }).one('transitionend', function() {
+            //动画结束后 
+            //hide() show() 连用时，浏览器会忽略hide 导致hide无效 使用offset断开这两个操作便可以使hide生效
+            $slides.hide().offset();
+            $slides.css({ transform: `translateX(${- (index+1) * 275}px)` }).show();
+        })
+    } else if (current === 0 && index === $images.length - 1) {
+        //从第一张到最后一张
+        $slides.css({ transform: 'translateX(0px)' }).one('transitionend', function() {
+            //动画结束后
+            //hide() show() 连用时，浏览器会忽略hide 导致hide无效 使用offset断开这两个操作便可以使hide生效
+            $slides.hide().offset();
+            $slides.css({ transform: `translateX(${- (index+1) * 275}px)` }).show();
+        })
+    } else {
+        $slides.css({ transform: `translateX(${- (index+1) * 275}px)` })
+    }
+    current = index;
 }
